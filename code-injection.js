77cd873ed22c041a418a610e97360233a7d8dba6
Alexander Caughey <alexander.caughey05@gmail.com>
2024-07-09 13:59:18 +0100

    Update code-injection.js

--- code-injection.js
+++ code-injection.js
@@ -1,5 +1,5 @@
-const userInput1 = "1;require('child_process').exec('cat /etc/passwd')"
+const userInputa = "1;require('child_process').exec('cat /etc/passwd')"
 eval(`var x = ${userInput}`)
 
-const userInput2 = "require('child_process').exec('cat /etc/passwd') && console.log"
+const userInputb = "require('child_process').exec('cat /etc/passwd') && console.log"
 var x = new Function(`return ${userInput}(a,b)`)
