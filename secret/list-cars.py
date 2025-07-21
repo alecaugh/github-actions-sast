0a8a7ab0a190a4a7548252e15fb567db8bb12b35
Alexander Caughey <alexander.caughey05@gmail.com>
2025-07-21 15:07:22 +0200

    add test github pat

---
+++ secret/list-cars.py
@@ -0,0 +1,33 @@
+import os
+import mysql.connector
+from fastapi import FastAPI
+
+app = FastAPI()
+
+def _get_db_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        database="CarDatabase",
+        password=os.environ.get("MYSQL_ROOT_PASSWORD"))
+
+
+@app.get("/")
+def read_root():
+    return { "message": "Hello World!"}
+
+
+@app.get("/cars")
+def read_cars():
+    conn = _get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM Cars")
+    cars = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return cars
+
+app.get("/test")
+def read_root():
+    secret = "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"
+    return { "message": "Hello World!"}
